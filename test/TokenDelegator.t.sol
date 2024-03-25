e4065acca28d072597692bdfb9d12ab82325a6a4
Uacias <filgum0326@gmail.com>
2024-03-25 16:15:43 +0100

    fix import

--- test/TokenDelegator.t.sol
+++ test/TokenDelegator.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import {TokenDelegator} from "../src/EmContract.sol";
+import {TokenDelegator} from "../src/TokenDelegator.sol";
 
 interface IMintableERC20 is IERC20 {
     function mint(address to, uint256 amount) external returns (bool);
